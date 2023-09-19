089094cbfb29a04e4781b9eb5c7363a12e9158fa
Red Onion <aliparvizi81.ap@gmail.com>
2023-09-19 11:29:04 +0330

    def tick_task and show_tasks also changing read_tasks and write_tasks

--- todolist.py
+++ todolist.py
@@ -4,17 +4,40 @@ import csv
 
 
 def main():
-    # get tasks and with ctrl + D end that:
-    tasks = get_tasks()
+    if len(sys.argv) == 3 and sys.argv[1] in ["-m", "--mark"]:
+        
+        # read tasks from file:
+        file_data = read_tasks("tasks.csv")
 
-    # write tasks:
-    write_tasks("tasks.csv", tasks)
-    
-    # read tasks:
-    tasks = read_tasks("tasks.csv")
+        # change tasks:
+        modified_data = tick_task(sys.argv[2], file_data)
 
-    # show tasks:
-    print(tabulate(tasks, tablefmt="grid"))
+        # write tasks in file:
+        write_tasks("tasks.csv", modified_data, file_exist=True)
+
+    if len(sys.argv) == 1:
+        # get tasks and with ctrl + D end that:
+        tasks = get_tasks()
+
+        # write tasks:
+        write_tasks("tasks.csv", tasks)
+        
+        # read tasks:
+        tasks = read_tasks("tasks.csv")
+
+        # show tasks:
+        print(show_tasks("tasks.csv")) 
+
+
+def tick_task(task, tasks):
+        counter = 0
+        for row in tasks:
+            if row["task"] == task:
+                row["tick"] = "ticked"
+                tasks.pop(counter)
+                tasks.insert(counter, row)
+                return tasks
+            counter += 1
 
 
 def get_tasks():
@@ -28,12 +51,20 @@ def get_tasks():
     return tasks
 
 
-def write_tasks(file_path, tasks):
+def write_tasks(file_path, tasks, file_exist=False):
     with open(file_path, "w",) as file:
         writer = csv.DictWriter(file, fieldnames=["tick", "task", "tag"])
         writer.writeheader()
-        for task in tasks:
-            writer.writerow({"tick": "unticked", "task": task, "tag": "None",})
+
+        # if file exist just update the changes
+        if file_exist:
+            for row in tasks:
+                writer.writerow({"tick": row["tick"], "task": row["task"], "tag": row["tag"],})
+        
+        # if it's a new file defult value for tick and tag and just write tasks in file
+        else:
+            for task in tasks:
+                writer.writerow({"tick": "unticked", "task": task, "tag": "None",})
 
 
 def read_tasks(file_path):
@@ -41,17 +72,27 @@ def read_tasks(file_path):
     with open(file_path) as file:
         reader = csv.DictReader(file)
         for row in reader:
-
-            # use Ballot Box ☐ for unticked
-            if row["tick"] == "unticked":
-                row["tick"] = "☐"
-
-            # use Ballot Box with Check ☑ for ticked
-            elif row["tick"] == "ticked":
-                row["tick"] == "☑"
-            
             tasks.append(row)
     return tasks
+
+
+def show_tasks(file_path):
+    file_data = read_tasks(file_path)
+    modified_data = []
+    for row in file_data:
+
+        # use Ballot Box ☐ for unticked
+        if row["tick"] == "unticked":
+            row["tick"] = "☐"
+
+        # use Ballot Box with Check ☑ for ticked
+        elif row["tick"] == "ticked":
+            row["tick"] == "☑"
+
+        modified_data.append(row)
+    
+    return tabulate(modified_data, tablefmt="grid")
+
 
 
 if __name__ == "__main__":
